d83592d96fe72fea66e433f2aece15f2b71eea41
minsoo9506 <ghktjd15gh@gmail.com>
2022-06-04 17:36:15 +0900

    feat: test를 위한 파일 작성

---
+++ tests/test_prediction.py
@@ -0,0 +1,19 @@
+# import math
+import numpy as np
+
+from fraud_detection_model.predict import make_prediction
+
+
+def test_make_prediction(sample_input_data):
+    expected_first_prediction_value = 0
+    expected_num_predictions = 71202
+
+    result = make_prediction(input_data=sample_input_data)
+
+    predictions = result.get("predictions")
+    assert isinstance(predictions, np.ndarray)
+    assert isinstance(predictions[0], np.float64)
+    assert result.get("errors") is None
+    assert len(predictions) == expected_num_predictions
+    assert predictions[0] == expected_first_prediction_value
+    # assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=0.1)
