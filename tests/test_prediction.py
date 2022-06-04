30e55553ee2c2bdc31ed34eb7ac62372523fb454
minsoo9506 <ghktjd15gh@gmail.com>
2022-06-04 23:55:32 +0900

    fix: argument 이름 수정 & test 조건 수정

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -4,15 +4,16 @@ import numpy as np
 from fraud_detection_model.predict import make_prediction
 
 
-def test_make_prediction(sample_input_data):
+def test_make_prediction(input_data):
     expected_first_prediction_value = 0
     expected_num_predictions = 71202
 
-    result = make_prediction(input_data=sample_input_data)
+    result = make_prediction(input_data=input_data)
 
     predictions = result.get("predictions")
     assert isinstance(predictions, np.ndarray)
-    assert isinstance(predictions[0], np.float64)
+    assert isinstance(predictions[0], np.int64)
+
     assert result.get("errors") is None
     assert len(predictions) == expected_num_predictions
     assert predictions[0] == expected_first_prediction_value
